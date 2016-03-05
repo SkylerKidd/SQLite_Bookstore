e9f9e79526ece742391143dc71052b5fb19d0835
Ruzanna Ayv <ruzanna.ayvazyan@yahoo.com>
2016-03-04 17:09:26 -0800

    Book population
    Populating the book in DB, still in progress

--- populate.sql
+++ populate.sql
@@ -8,6 +8,43 @@ INSERT INTO BOOK VALUES('978-0385743570','Paperback','Steelheart','Brandon Sande
 INSERT INTO BOOK VALUES('978-1423176374','Hardcover','All Our Yesterdays','Cristin Terrill');
 INSERT INTO BOOK VALUES('978-1616203221','Paperback','Life After Life','Jill McCorkle');
 INSERT INTO BOOK VALUES('978-0316176491','Paperback','Life After Life','Kate Atkinson');
+INSERT INTO BOOK VALUES('978-0553293357','Paperback','Foundation','Isaac Asimov');
+INSERT INTO BOOK VALUES('978-0486411095','Hardcover','Dracula','Bram Stoker');
+INSERT INTO BOOK VALUES('978-1451645859','Paperback','Ordinary Grace','William Kent Krueger');
+INSERT INTO BOOK VALUES('978-0312577223','Hardcover','The Nightingale','Kristin Hannah');
+INSERT INTO BOOK VALUES('978-1616149987','Paperback','The Life We Bury','Allen Eskens');
+INSERT INTO BOOK VALUES('978-1477820018','Paperback','Take Me With You','Catherine Ryan Hyde');
+INSERT INTO BOOK VALUES('978-0451419927','Paperback','Secrets of a Charmed Life','Susan Meissner');
+INSERT INTO BOOK VALUES('978-1250074355','Paperback','The Silent Sister','Diane Chamberlain');
+INSERT INTO BOOK VALUES('978-0143127550','Paperback','Everything I Never Told You','Celeste Ng');
+INSERT INTO BOOK VALUES('978-0758278456','Paperback','What She Left Behind','Ellen M. Wiseman');
+INSERT INTO BOOK VALUES('978-1477822081','Paperback','Trail of Broken Wings','Sejal Badani');
+INSERT INTO BOOK VALUES('978-1594633669','Hardcover','The Girl on the Train','Paula Hawkins');
+INSERT INTO BOOK VALUES('978-1507704530','Paperback','The Way We Fall','Cassia Leo');
+INSERT INTO BOOK VALUES('978-1611099799','Paperback','When I Found You','Catherine Ryan Hyde');
+INSERT INTO BOOK VALUES('978-0804171472','Paperback','The Narrow Road to the Deep North','Richard Flanagan');
+INSERT INTO BOOK VALUES('978-1451681758','Paperback','The Light Between Oceans','M.L. Stedman');
+INSERT INTO BOOK VALUES('978-0143121701','Paperback','The Invention of Wings','Sue Monk Kidd');
+INSERT INTO BOOK VALUES('978-0812993547','Hardcover','Between the World and Me','Ta-Nehisi Coates');
+INSERT INTO BOOK VALUES('978-0812988406','Hardcover','When Breath Becomes Air','Deckle Edge');
+INSERT INTO BOOK VALUES('978-1503944435','Paperback','The Good Neighbor','A. J. Banner');
+INSERT INTO BOOK VALUES('978-0615590585','Paperback','The Pecan Man','Cassie D. Selleck');
+INSERT INTO BOOK VALUES('978-0553418026','Paperback','The Martian','Andy Weir');
+INSERT INTO BOOK VALUES('978-0547577319','Paperback','A Long Walk to Water','Linda Sue Park');
+INSERT INTO BOOK VALUES('978-0062315007','Paperback','The Alchemist','Paulo Coelho');
+INSERT INTO BOOK VALUES('978-0451419910','Paperback','A Fall of Marigolds','Susan Meissner');
+INSERT INTO BOOK VALUES('978-0062413864','Paperback','What She Knew','Gilly Macmillan');
+INSERT INTO BOOK VALUES('','','','');
+INSERT INTO BOOK VALUES('','','','');
+INSERT INTO BOOK VALUES('','','','');
+INSERT INTO BOOK VALUES('','','','');
+INSERT INTO BOOK VALUES('','','','');
+INSERT INTO BOOK VALUES('','','','');
+INSERT INTO BOOK VALUES('','','','');
+INSERT INTO BOOK VALUES('','','','');
+INSERT INTO BOOK VALUES('','','','');
+INSERT INTO BOOK VALUES('','','','');
+
 
 -- LISTING INSERTS
 INSERT INTO LISTING VALUES('skyler','978-0385743563','24.99','14');
